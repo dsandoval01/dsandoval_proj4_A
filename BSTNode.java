8303aed1950acd4bbb92a472b34d8e17047a26d4
Danna Paola Sandoval Sánchez <dsandoval@conncoll.edu>
2022-11-07 19:38:51 -0500

    test

---
+++ BSTNode.java
@@ -0,0 +1,18 @@
+class BSTNode{
+	public BSTNode parent;
+	public int element;
+	public BSTNode left;
+	public BSTNode right;
+
+	public BSTNode(BSTNode p, int e, BSTNode l, BSTNode r){
+		parent = p;
+		element = e;
+		left = l;
+		right = r;
+	}
+
+	// public void displayNode{
+
+	// }
+
+}
